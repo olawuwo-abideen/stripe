fe238528f91eacffac6102f529c5b65c56b7285a
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-07-25 12:41:09 +0100

    server

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
-
-
-
 app.use(express.json());
+
+// controller
+const stripeController = require('./controllers/stripe');
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 
 // routes
 app.get('/', (req, res) => {
   res.send('<h1> Stripe Api</h1>');
 });
 
-
+app.post('/stripe', stripeController);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 
 
